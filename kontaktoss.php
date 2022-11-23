ca9708a22b6f922327448b4ab965b40f633ccbf6
RamarrZanerni <112287311+RamarrZanerni@users.noreply.github.com>
2022-11-23 01:05:53 +0100

    Add files via upload

---
+++ kontaktoss.php
@@ -0,0 +1,20 @@
+<?php
+if ( isset($_POST['submit'])) {
+
+    $name=$_POST['name'];
+    $mailform=$_POST['mail'];
+    $melding=$_POST['melding'];
+
+    
+
+   $mailto=   "rama198259@gmail.com";
+   $headers= "form: ".$mailform;
+   $txt ="du har mottatt en e-post fra".$name.".\n\n".$melding;
+
+   mail($mailto, $txt, $headers);
+   header("location: kontaktoss.html?mailsend")
+
+}
+?>
+
+
